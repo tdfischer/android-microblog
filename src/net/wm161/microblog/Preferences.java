66a76a88b0cb9859070f076eade257e11bbba05b
Trever Fischer <wm161@wm161.net>
2010-03-21 21:33:44 -0400

    Add versioning to the preferences system

--- src/net/wm161/microblog/Preferences.java
+++ src/net/wm161/microblog/Preferences.java
@@ -3,18 +3,75 @@ package net.wm161.microblog;
 import java.util.UUID;
 
 import net.wm161.microblog.lib.Account;
-
-
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.util.Log;
 
 public class Preferences {
 		SharedPreferences m_prefs;
 		MicroblogApp m_app;
+		public final int VERSION = 4;
 
 		public Preferences(Context cxt) {
 			m_prefs = cxt.getSharedPreferences("Microblogging.Main", Context.MODE_PRIVATE);
 			m_app = (MicroblogApp)cxt.getApplicationContext();
+			upgrade();
+		}
+		
+		public boolean upgrade() {
+			int configVersion;
+			while((configVersion = m_prefs.getInt("version", 0)) < VERSION) {
+				Log.d("Preferences", "Current version: " + configVersion);
+				if (!upgrade(configVersion))
+					return false;
+			}
+			return true;
+		}
+		
+		private void writeVersion(int version) {
+			m_prefs.edit().putInt("version", version).commit();
+			Log.d("Preferences", "Version set to "+version);
+		}
+		
+		//TODO: Test this with the data from the market version
+		public boolean upgrade(int old) {
+			Log.d("Preferences", "Upgrading to "+VERSION+" from "+old);
+			switch(old) {
+				case 0:
+					if (m_prefs.contains("accounts"))
+						writeVersion(3);
+					else
+						writeVersion(VERSION);
+					break;
+				case 3:
+					//In version 0.1 (serial 3), accounts stored a URL in baseurl.
+					String accountGUIDs = m_prefs.getString("accounts", null);
+					String[] guids = accountGUIDs.split(",");
+					for(String acct : guids) {
+						String baseurl = m_prefs.getString(acct+".baseurl", "");
+						Uri base = Uri.parse(baseurl);
+						
+						Editor editor = m_prefs.edit();
+						editor.putString(acct+".api", "statusnet");
+						editor.putInt(acct+".apiversion", 1);
+						editor.remove(acct+".baseurl");
+						
+						String server = base.getHost();
+						String path = base.getPath();
+						boolean https = base.getScheme().equals("https");
+						
+						editor.putString(acct+".api.server", server);
+						editor.putString(acct+".api.path", path);
+						editor.putBoolean(acct+".api.https", https);
+						
+						editor.commit();
+					}
+					writeVersion(4);
+					break;
+			}
+			return true;
 		}
 
 		public void setDefaultAccount(String guid) {
