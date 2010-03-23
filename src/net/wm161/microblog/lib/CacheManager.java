c801b1707713f4c404bf78b51dafa22099d40fb5
Trever Fischer <wm161@wm161.net>
2010-03-23 00:51:46 -0400

    Add versioning to the cache manager, to invalidate future 'corrupt' caches.

--- src/net/wm161/microblog/lib/CacheManager.java
+++ src/net/wm161/microblog/lib/CacheManager.java
@@ -1,16 +1,20 @@
 package net.wm161.microblog.lib;
 
 import java.io.File;
-import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
-
-
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import android.content.Context;
 import android.util.Log;
 
 public class CacheManager {
+	public static final int CACHE_VERSION = 1;
+	
 	public enum CacheType {
 		Avatar,
 		Status,
@@ -23,6 +27,33 @@ public class CacheManager {
 	public CacheManager(Context cxt) {
 		m_cxt = cxt;
 		m_caches = new HashMap<Account, HashMap<CacheType, DataCache<? extends Comparable<?>, ? extends Serializable>>>();
+		File cacheVersion = new File(m_cxt.getCacheDir()+File.pathSeparator+"version");
+		int version = CACHE_VERSION;
+		try {
+			Scanner scanner = new Scanner(cacheVersion);
+			version = scanner.nextInt();
+			if (version != CACHE_VERSION) {
+				Log.i("CacheManager", "Old version of cache detected. Cleaning up...");
+				updateVersion();
+			}
+		} catch (FileNotFoundException e) {
+			updateVersion();
+		} catch (NoSuchElementException e) {
+			updateVersion();
+		}
+	}
+	
+	private void updateVersion() {
+		clear();
+		File cacheVersion = new File(m_cxt.getCacheDir()+File.pathSeparator+"version");
+		FileWriter writer;
+		try {
+			writer = new FileWriter(cacheVersion);
+			writer.write(String.valueOf(CACHE_VERSION));
+			Log.i("CacheManager", "Cache updated to version "+CACHE_VERSION);
+		} catch (IOException e) {
+			Log.e("CacheManager", "Couldn't create cache version file.");
+		}
 	}
 	
 	public <K extends Comparable<K>, V extends Serializable> DataCache<K, V> getCache(Account acct, CacheType type) {
