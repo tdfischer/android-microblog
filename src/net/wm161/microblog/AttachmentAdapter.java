6196ec50203dc07ea4649c07aff12f6b59c7c468
Trever Fischer <wm161@wm161.net>
2010-04-01 01:06:28 -0400

    Import cleanup

--- src/net/wm161/microblog/AttachmentAdapter.java
+++ src/net/wm161/microblog/AttachmentAdapter.java
@@ -28,7 +28,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 
 public class AttachmentAdapter extends BaseAdapter {
 	
